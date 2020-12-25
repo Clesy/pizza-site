9f17079f7b0d64d3583bcd221c619ee5619ae0db
Evgeniy <gerasum04@gmail.com>
2020-12-25 17:30:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let slideAutoIndex = 0;
+showAutoSlides();
+
+function plusSlides(n) {
+    showAutoSlides(slideAutoIndex += n);
+}
+
+function currentSlide(n) {
+    showAutoSlides(slideAutoIndex = n)
+
+}
+
+function showAutoSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slides");
+    /*AUTO SLIDER */
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideAutoIndex++;
+    if (slideAutoIndex > slides.length) {
+        slideAutoIndex = 1
+    }
+    slides[slideAutoIndex - 1].style.display = "block";
+    setTimeout(showAutoSlides, 20000);
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
